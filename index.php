bfa04a80e986561d14920ec5654d8c74bd3c91ce
Анастасия Лабуть <anlab@mail.ru>
2018-11-26 21:34:15 +0300

    Функция подсчета задач в проектах - доработки

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
+declare(strict_types=1);
+
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
 
 $category = ['Входящие', 'Учеба', 'Работа', 'Домашние дела', 'Авто'];
 $tasks = [['name' => 'Собеседование в IT компании', 'dateOfCompletion' => '01.12.2018', 'category' => 'Работа', 'isCompleted' => false], ['name' => 'Выполнить тестовое задание', 'dateOfCompletion' => '25.12.2018', 'category' => 'Работа', 'isCompleted' => false], ['name' => 'Сделать задание первого раздела', 'dateOfCompletion' => '21.12.2018', 'category' => 'Учеба', 'isCompleted' => true], ['name' => 'Встреча с другом', 'dateOfCompletion' => '22.12.2018', 'category' => 'Входящие', 'isCompleted' => false], ['name' => 'Купить корм для кота', 'dateOfCompletion' => '', 'category' => 'Домашние дела', 'isCompleted' => false], ['name' => 'Заказать пиццу', 'dateOfCompletion' => '', 'category' => 'Домашние дела', 'isCompleted' => false]];
 
-function count_tasks($list_tasks, $project) {
+function count_tasks(array $list_tasks, string $project) : int {
     $i = 0;
     
-    foreach ($list_tasks as $key => $value) {
+    foreach ($list_tasks as $value) {
         if (isset($value['category']) && $value['category'] === $project) {
             $i++;
         }
