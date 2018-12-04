008579074fa46273a84c47f66efb7e6910e307d9
Анастасия Лабуть <anlab@mail.ru>
2018-12-04 23:20:51 +0300

    SQL-запросы

---
+++ queries.sql
@@ -0,0 +1,42 @@
+-- Добавление списка проектов
+
+INSERT INTO projects SET name = 'Входящие', user_id = 1;
+INSERT INTO projects SET name = 'Учеба', user_id = 1;
+INSERT INTO projects SET name = 'Работа', user_id = 1;
+INSERT INTO projects SET name = 'Домашние дела', user_id = 1;
+INSERT INTO projects SET name = 'Авто', user_id = 1;
+
+-- Добавление пользователей
+
+INSERT INTO users SET email = 'anlab@mail.ru', name = 'Анастасия', password = 'xcdfrt56';
+INSERT INTO users SET email = 'evgeniy2@mail.ru', name = 'Евгений', password = 'bnhjui89';
+INSERT INTO users SET email = 'olga3@mail.ru', name = 'Ольга', password = 'cvdfer34';
+
+-- Добавление список задач
+
+INSERT INTO tasks SET completed_status = 0, name = 'Собеседование в IT компании', deadline = '02.12.2018', user_id = 1, project_id = 3;
+INSERT INTO tasks SET completed_status = 0, name = 'Выполнить тестовое задание', deadline = '25.12.2018', user_id = 1, project_id = 3;
+INSERT INTO tasks SET completed_status = 1, name = 'Сделать задание первого раздела', deadline = '21.12.2018', user_id = 1, project_id = 2;
+INSERT INTO tasks SET completed_status = 0, name = 'Встреча с другом', deadline = '22.12.2018', user_id = 1, project_id = 1;
+INSERT INTO tasks SET completed_status = 0, name = 'Купить корм для кота', user_id = 1, project_id = 4;
+INSERT INTO tasks SET completed_status = 0, name = 'Заказать пиццу', user_id = 1, project_id = 4;
+
+-- Получить список из всех проектов для одного пользователя
+
+SELECT name FROM projects WHERE user_id = 1;
+
+-- Получить список из всех задач для одного проекта
+
+SELECT name FROM tasks WHERE project_id = 3;
+
+-- Пометить задачу как выполненную
+
+UPDATE tasks SET completed_status = 1 WHERE id = 4;
+
+-- Получить все задачи для завтрашнего дня
+
+SELECT name, deadline FROM tasks WHERE deadline = '04.12.2018';
+
+-- Обновить название задачи по её идентификатору
+
+UPDATE tasks SET name = 'Купить обратный билет на Ласточку' WHERE id = 5;
