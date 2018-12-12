8ebb7ce985e81aa42a9d3b4acdcb95a5ca15e71d
Анастасия Лабуть <anlab@mail.ru>
2018-12-12 17:52:47 +0300

    Работа со строкой запроса - доработки - 2

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ function include_template(string $name, array $data) : string {
     return $result;
 }
 
-function count_tasks(array $list_tasks, string $project) : int {
+function count_tasks(array $list_tasks, int $project) : int {
     $i = 0;
     
     foreach ($list_tasks as $value) {
@@ -42,36 +42,31 @@ function isLessThanDay(string $dateOfCompletion) : bool {
 function connectToMysql() {
     $link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     mysqli_set_charset($link, "utf8");
+    if (!$link) {
+        die(mysqli_connect_error());
+    }
     return $link;
 }
 
 function getProjects(int $id) : array {
     $link = connectToMysql();
-    if (!$link) {
-        die(mysqli_connect_error());
-    } else {
-        $sql = 'SELECT * FROM projects WHERE user_id = ?';
-        $stmt = db_get_prepare_stmt($link, $sql, [$id]);
-        $categories = db_get_result_stmt($stmt);
-        mysqli_close($link);
-        return $categories;
-    }    
+    $sql = 'SELECT * FROM projects WHERE user_id = ?';
+    $stmt = db_get_prepare_stmt($link, $sql, [$id]);
+    $categories = db_get_result_stmt($stmt);
+    mysqli_close($link);
+    return $categories;
 }
 
 function getTasks(int $userId, int $projectId = null) : array {
     $link = connectToMysql();
-    if (!$link) {
-        die(mysqli_connect_error());
-    } else {
-        $sql = 'SELECT * FROM tasks WHERE user_id = ?';
-        $arguments = [$userId];
-        if ($projectId !== null) {
-            $sql .= ' AND WHERE project_id = ?';
-            $arguments[] = $projectId;
-        }
-        $stmt = db_get_prepare_stmt($link, $sql, $arguments);
-        $tasks = db_get_result_stmt($stmt);
-        mysqli_close($link);
-        return $tasks;
+    $sql = 'SELECT * FROM tasks WHERE user_id = ?';
+    $arguments = [$userId];
+    if ($projectId !== null) {
+        $sql .= ' AND project_id = ?';
+        $arguments[] = $projectId;
     }
+    $stmt = db_get_prepare_stmt($link, $sql, $arguments);
+    $tasks = db_get_result_stmt($stmt);
+    mysqli_close($link);
+    return $tasks;
 }
